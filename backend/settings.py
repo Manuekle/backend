2a82ef5cab10f0df63279197f84de3b200d149f3
Manuel Erazo <elustondo129@gmail.com>
2022-08-18 17:25:39 -0500

    amazon web services

--- backend/settings.py
+++ backend/settings.py
@@ -127,29 +127,29 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'manganiacos_db',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'manganiacos_db',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'manganiacos',
-#        'USER': 'postgres',
-#        'PASSWORD': 'knEQC15CCDQ1xNeE7TAY',
-#        'HOST': 'dbmanganiacos.cn2nsfxq6hjk.us-east-1.rds.amazonaws.com',
-#        'PORT': '5432'
-#    }
-# }
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'manganiacos',
+       'USER': 'postgres',
+       'PASSWORD': 'knEQC15CCDQ1xNeE7TAY',
+       'HOST': 'dbmanganiacos.cn2nsfxq6hjk.us-east-1.rds.amazonaws.com',
+       'PORT': '5432'
+   }
+}
 
 
 # Password validation
